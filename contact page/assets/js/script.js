0a5bd688a30c6653c5b4ca98d8cd03307a2e2aff
Abdul Rehman <arplays03@gmail.com>
2023-10-07 21:29:32 +0500

    Responsiveness

---
+++ contact page/assets/js/script.js
@@ -0,0 +1,21 @@
+// var swiper = new Swiper(".mySwiper", {
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     slidesPerView: 3,
+//     spaceBetween: 30,
+//     loop: true,
+//     freeMode: true,
+//     breakpoints: {
+//         400: {
+//            slidesPerView: 1,
+//         },
+//         500: {
+//             slidesPerView: 2,
+//         },
+//         860: {
+//             slidesPerView: 3,
+//         },
+//     }
+//   });
